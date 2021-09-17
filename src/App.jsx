56820650d6677e50e55016d6c3c518206f4fa8fd
toshiakifukikoshi <86909359+toshiakifukikoshi@users.noreply.github.com>
2021-09-17 19:18:28 +0900

    17日夜まで

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfullMessage from "./components/ColorfullMessage";
 
 const App = () => {
+  useState();
   const onCLickButton = () => {
     alert();
   };
